e4cf644a988c82a3e460c8db77941510ebccb5a7
Narendran Bhaskaran <narendrabhaskar999@gmail.com>
2025-03-11 09:12:16 +0100

    finished Product Module and User Management

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('home/',views.index,name='home'),
     path('product_list/',views.list_products,name='list_product'),
-    path('product_detail',views.detail_product,name='detail_product')
+    path('product_detail/<pk>',views.detail_product,name='detail_product')
 ]
 
