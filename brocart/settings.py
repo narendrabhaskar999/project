f80e591b38e8540fe68e6c6e89bf31f078921a49
Narendran Bhaskaran <narendrabhaskar999@gmail.com>
2025-04-04 23:50:46 +0200

    changed the suggestions

--- brocart/settings.py
+++ brocart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +79,19 @@ WSGI_APPLICATION = 'brocart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+load_dotenv()
+
+print("ENV USER:",os.getenv("DATABASE_USER"))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD':os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT':os.getenv('DATABASE_PORT'),
+        
     }
 }
 
--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('home/',views.index,name='home'),
     path('product_list/',views.list_products,name='list_product'),
-    path('product_detail/<pk>',views.detail_product,name='detail_product')
+    path('product_detail/<pk>',views.detail_product,name='detail_product'),
+    path('',views.index,name='home')
 ]
 
